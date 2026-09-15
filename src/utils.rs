1c77f0ee8f534d182a986756b5609e9e0a48c3e0
Radonoxius <Radonoxius@protonmail.com>
2026-09-15 18:43:55 +0530

    Modified get_application_name comment

--- src/utils.rs
+++ src/utils.rs
@@ -47,7 +47,7 @@ pub unsafe fn enumerate_instance_extensions(
     }
 }
 
-/// Returns the name of the Application or `Unnamed` if name isnt valid UTF-8.
+/// Returns the name of the Application or "Unnamed" if name isnt valid UTF-8.
 /// 
 /// SAFETY: Application Name must be null terminated!
 pub unsafe fn get_application_name<'a>(
